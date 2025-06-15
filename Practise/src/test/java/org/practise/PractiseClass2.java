ef7a83558f86004bd9ef5286c73f5a4d0073040e
Prathamesh Ingale <prathameshingale@Prathameshs-MacBook-Air.local>
2025-06-15 20:28:50 +0530

    Learnings of logging and Hamcrest matchers

--- Practise/src/test/java/org/practise/PractiseClass2.java
+++ Practise/src/test/java/org/practise/PractiseClass2.java
@@ -2,6 +2,9 @@ package org.practise;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.Collections;
+
 import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
@@ -13,7 +16,7 @@ import static io.restassured.RestAssured.given;
 public class PractiseClass2 {
 
 
-    @Test(description = "This test is to explore simple assertThat methods from hamcrest")
+    @Test(description = "This test is to explore simple assertThat methods from hamcrest" , enabled = false)
     public void hamcrest_asserts_on_extracted_response() {
 
         String firstWorkspaceName =
@@ -33,6 +36,89 @@ public class PractiseClass2 {
         Assert.assertEquals("My Workspace" , firstWorkspaceName); // TestNG Assertion
 
     }
+
+    @Test(description = "Contains validates each value and order. It has to have all elements and to be in right order" , enabled = false)
+    public void hamcrest_asserts_contains() {
+
+                given().
+                        baseUri("https://api.postman.com").
+                        header("x-api-key", ConfigUtil.getApiKey()).
+                when().
+                        get("/workspaces").
+                then().
+                        log().all().
+                        assertThat().
+                        statusCode(200).
+                        body("workspaces.name" , contains("My Workspace" , "Team Workspace" , "User_Managemnt_team" , "QA")).
+                        body("workspaces.name" , containsInAnyOrder( "QA" , "My Workspace" , "Team Workspace" , "User_Managemnt_team" ));
+    }
+
+    @Test(description = "Checking response size, is empty or not, verifying has particular element" , enabled = false)
+    public void few_handy_assertions_Byhamecrest() {
+
+        given().
+                baseUri("https://api.postman.com").
+                header("x-api-key", ConfigUtil.getApiKey()).
+        when().
+                get("/workspaces").
+        then().
+                log().all().
+                assertThat().
+                statusCode(200).
+                body("workspaces.name" , containsInAnyOrder(  "My Workspace" , "QA" , "Team Workspace" , "User_Managemnt_team"  ) ,
+                        "workspaces.name" , is(not(empty())) ,
+                        "workspaces.name" , hasSize(4) ,
+                        "workspaces.name" , is(not(emptyArray())) ,
+                        "workspaces.name" , hasItem("QA"));
+       //               "workspaces.name" , everyItem(startsWith("My"))) ;
+
+    }
+
+    @Test(description = "Considering repsonse in Key and value pair" , enabled = false)
+    public void few_more_handy_assertions_Byhamecrest() {
+
+        given().
+                baseUri("https://api.postman.com").
+                header("x-api-key", ConfigUtil.getApiKey()).
+        when().
+                get("/workspaces").
+        then().
+                log().all().
+                assertThat().
+                statusCode(200).
+                body("workspaces[0]" , hasKey("name") ,
+                        "workspaces[0]" ,hasValue("My Workspace") ,
+                                 "workspaces[0]" , hasEntry("id" , "a1befd3a-f6ca-4256-848f-23fdcf00b91c"));
+          //                     "workspaces[0]" , equalTo(Collections.EMPTY_MAP));
+    }
+
+    @Test(description = "Works on string. anyOf - At least one of these & allOf All of these" )
+    public void allof_anyof_matchers_byHamcrest() {
+
+        given().
+                baseUri("https://api.postman.com").
+                header("x-api-key", ConfigUtil.getApiKey()).
+        when().
+                get("/workspaces").
+        then().
+                log().all().
+                assertThat().
+                statusCode(200).
+                body("workspaces[0].name" , allOf(startsWith("My") , containsString("Workspace")) ,
+               "workspaces[0].name" , anyOf(startsWith("jdfhkjf") , containsString("Workspace")));
+
+
+    }
+
+
+
+
+
+
+
+
+
+
 
 
 }
---
+++ Practise/src/test/java/org/practise/PractiseClass3Logging.java
@@ -0,0 +1,90 @@
+package org.practise;
+
+import io.restassured.config.LogConfig;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+public class PractiseClass3Logging {
+
+    @Test(description = "We can log required thing in both request and response" , enabled = false)
+    public void few_handy_assertions_Byhamecrest() {
+
+        given().
+                baseUri("https://api.postman.com").
+                header("x-api-key", ConfigUtil.getApiKey()).
+                log().all().        // To log all everything in the request
+                log().headers().    // To log all headers
+                log().parameters(). // to log all parameters
+        when().
+                get("/workspaces").
+        then().
+                log().all().       // To log everything in response
+                log().body().      // To log body
+                log().cookies().   // To log cookies in response
+                log().headers().   // to log headers in response
+                assertThat().
+                statusCode(200);
+    }
+
+    @Test(description = "Using ifError method " , enabled = false)
+    public void logOnly_ifErrors() {
+
+        given().
+                baseUri("https://api.postman.com").
+                header("x-api-key", ConfigUtil.getApiKey()).
+        when().
+                get("/workspaces").
+        then().
+                log().ifError().     // Only if we have error Example - Wrong authenticate, Syntax error
+                assertThat().
+                statusCode(200);
+    }
+
+    @Test(description = "Using ifValidationFails method " , enabled = false )
+    public void logOnly_if_validationFails() {
+
+        given().
+                baseUri("https://api.postman.com").
+                header("x-api-key", ConfigUtil.getApiKey()).
+                log().ifValidationFails().   // To get request if validation fails
+        //        config(config.logConfig(LogConfig.logConfig().enableLoggingOfRequestAndResponseIfValidationFails()));
+        when().
+                get("/workspaces").
+        then().
+                log().ifValidationFails().    // Only if added valiadation here status code fails
+                assertThat().
+                statusCode(200);
+
+        System.out.println("Instead of writting 2 times log().ifValidationFails()");
+        System.out.println("Use config(config.logConfig(LogConfig.logConfig().enableLoggingOfRequestAndResponseIfValidationFails())) ");
+
+    }
+
+    @Test(description = "Using config blacklistHeader() method" )
+    public void hiding_sensitive_from_logging_headers() {
+
+        given().
+                baseUri("https://api.postman.com").
+                header("x-api-key", ConfigUtil.getApiKey()).
+                config(config.logConfig(LogConfig.logConfig().blacklistHeader("x-api-key"))).
+                log().all().
+         when().
+                get("/workspaces").
+        then().
+                log().all().
+                assertThat().
+                statusCode(403);
+
+        System.out.println("If we have multiple attributes to hide, We can send Collection like Set of strings");
+
+    }
+
+
+
+
+
+
+
+}
